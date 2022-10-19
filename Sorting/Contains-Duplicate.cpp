76f526a6e6f0a21ab0352ce76c0396a0984b914a
shubhanshurav <shubhanshurav63@gmail.com>
2022-10-19 10:02:38 +0530

    Contains-Duplicate

---
+++ Sorting/Contains-Duplicate.cpp
@@ -0,0 +1,10 @@
+bool containsDuplicate(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
