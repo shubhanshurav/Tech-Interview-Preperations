110e322803c8bd14c53813203fd6b9b0aefa5c2b
shubhanshurav <shubhanshurav63@gmail.com>
2022-10-20 10:00:11 +0530

    Find Middle element in Linked List

---
+++ Linked-List/Find-Middle-Element-in-Linked-List.cpp
@@ -0,0 +1,35 @@
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        
+        if(head == NULL) 
+        return NULL;
+           
+        Node *slow = head;
+        Node *fast = head;
+        
+        while(fast != NULL && fast->next != NULL){
+            
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            
+        }
+        
+        return slow->data;
+        
+    }
+};
