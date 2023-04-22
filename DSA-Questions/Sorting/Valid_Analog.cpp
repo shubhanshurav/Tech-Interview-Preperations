6524cd687e8176a8e6dc29b96b4ce92da76ea3e1
shubhanshurav <shubhanshurav63@gmail.com>
2023-04-22 12:06:56 +0530

    update questions

--- DSA-Questions/Sorting/Valid_Analog.cpp
+++ DSA-Questions/Sorting/Valid_Analog.cpp
@@ -2,49 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        // <------  Approach - 01 ------------>
-        //make a frequency Table array of size 256 
-        //and initialize all with 0
-        int freqTable[256] = {0};
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
 
-        //add freq of each char of string s into freqtable
-        for(int i=0;i<s.size();i++){
-            freqTable[s[i]]++;
-        }
-        //decreament freq of each char of string t into freqtable
-        for(int i=0;i<t.size();i++){
-            freqTable[t[i]]--;
-        }
-
-        for(int i=0;i<256;i++){
-            //agar freq table 0 nhi hai to false return kr do
-           if(freqTable[i] != 0)
-                return false;
+        if(s.size() == t.size()){
+            for(int i=0;i<s.size();i++){
+                if(s[i] == t[i]){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }else{
+            return false;
         }
 
-        return true;
-
-        //   <----------- Another way to write this code --------------->
-        
-     /*   int freqTable1[256] = {0};
-        int freqTable2[256] = {0};
-
-        for(int i=0;i<s.size();i++){
-            freqTable1[s[i]]++;
-        }
-
-        for(int i=0;i<t.size();i++){
-            freqTable2[t[i]]++;     
-        }
-
-        for(int i=0;i<256;i++){
-           if(freqTable1[i]==freqTable2[i]){
-                continue;
-            } else{
-                return false;
-            } 
-        }
-        return true;
-    */
+    return true;
     }
 };
