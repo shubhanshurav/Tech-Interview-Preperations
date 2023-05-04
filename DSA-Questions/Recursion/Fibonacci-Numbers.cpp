e87577e04f5fa0201f9d725ef5ce2e72661df5e5
shubhanshurav <shubhanshurav63@gmail.com>
2023-05-04 15:49:50 +0530

    Upload Questions

--- DSA-Questions/Recursion/Fibonacci-Numbers.cpp
+++ DSA-Questions/Recursion/Fibonacci-Numbers.cpp
@@ -1,12 +1,20 @@
-int fib(int n) {
-        //base case
-        if(n<=0){
-            return 0;
-        }           
-        
-        if(n==1){
-            return 1;
-        }
-            
-        return fib(n-1)+fib(n-2);
-    }
+#include<iostream>
+using namespace std;
+
+
+int fib(int n){
+    
+    //base case
+    if(n==0) return 0;
+    if(n==1) return 1;
+
+    //recursive relation
+    return fib(n-1)+fib(n-2);
+}
+
+int main(){
+
+    cout << fib(9);
+
+    return 0;
+}
