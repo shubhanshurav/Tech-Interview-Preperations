c36b06a771597a1a9ba679513ae31dadb4bfce99
shubhanshurav <shubhanshurao7037@gmail.com>
2023-06-11 08:29:53 +0530

    Upload solution

--- DSA-Questions/Sorting/merge-intervals.cpp
+++ DSA-Questions/Sorting/merge-intervals.cpp
@@ -31,22 +31,26 @@ intervals[i].length == 2
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> mergeIntervals;
-        if(intervals.size() == 0) return mergeIntervals;
-
-        sort(intervals.begin(),intervals.end());
-
-        vector<int> tempIntervals = intervals[0];
-
-        for(auto it:intervals){
-            if(it[0] <= tempIntervals[1]){
-                tempIntervals[1] = max(it[1], tempIntervals[1]);
-            }else{
-                mergeIntervals.push_back(tempIntervals);
-                tempIntervals = it;
+     vector<vector<int>> result;
+        //base case when threr are no intervals
+        if(interval.size()==0)return result;
+        //sort takes O(nlogn) time
+        sort(interval.begin(),interval.end());
+        //insert the the the first interval in the result
+        result.push_back(interval[0]);
+        int j=0;
+        //Traverse the whole vector .Takes O(n) time
+        for(int i=1;i<interval.size();i++)
+        {
+            //if intevals are overlapping
+            if(result[j][1]>=interval[i][0]) result[j][1]=max(result[j][1],interval[i][1]);
+            //else they are not overlapping
+            else
+            {
+                j++;
+                result.push_back(interval[i]);
             }
         }
-        mergeIntervals.push_back(tempIntervals);
-        return mergeIntervals;
+        return result;
     }
 };
